3e7ada69e346c31782677d4f99345f06c2fdb55f
chriszhendev <136290178+chriszhendev@users.noreply.github.com>
2024-08-20 23:57:41 -0400

    update

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21bfdb42a24624c96e5343a066bee118cc65bf14")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b08b411c6f0000b916ae34cbff38e3172a40beb")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
